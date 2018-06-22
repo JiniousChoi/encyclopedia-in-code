3be2f283b5dc6acabfca261de3a9108aa901f89a
Jin Choi <jinchoiseoul@gmail.com>
2018-06-23 00:01:51 +0900

    finish chapter 1

--- languages/haskell/programming-in-haskell.hs
+++ languages/haskell/programming-in-haskell.hs
@@ -1,12 +1,40 @@
--- Chapter 01
+#!/usr/bin/env runhaskell
+-- usage 1 (as script) : ./script.hs
+-- usage 2 (as script) : runhaskell script.hs
+-- usage 3 (compile) : ghc -o hello script.hs && ./hello
+-- uwage 4 (in repl) : ghci script.hs
+-- usage 5 (in repl) : guci
+--                     Prelude> :load script.hs
+
 
 main = do
-    --putStrLn $ "double 3 = " ++ (show $ double 3)
-    --putStrLn $ "sum [1,2,3] = " ++ (show $ sum [1,2,3])
-    showStmt "double 3" (double 3)
-    showStmt "sum [1,2,3]" (sum [1,2,3])
+    -- Chapter 01
+    printEq "double 3" $ double 3
+    printEq "sum [1,2,3]" $ sum [1,2,3]
+    printEq "sum' [1,2,3]" $ sum' [1,2,3]
+    printEq "qsort [2,1,4,1,3]" $ qsort [2,1,4,1,3]
+    printEq "qsort \"abracadabra\"" $ qsort "abracadabra"
+    res <- seqn [getChar, getChar, getChar]
+    putStrLn res
 
-showStmt :: Show a => [Char] -> a -> IO ()
-showStmt s a = do putStrLn $ s ++ " = " ++ (show a)
+-- Chapter 01
+printEq :: Show a => [Char] -> a -> IO ()
+printEq s a = putStrLn $ s ++ " = " ++ (show a)
 
 double x = x + x
+
+sum' [] = 0
+sum' (n:ns) = n + sum'(ns)
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+               where smaller = [a|a <- xs, a <= x]
+                     larger  = [b|b <- xs, b > x ]
+
+-- seqn :: [IO a] -> IO [a]
+seqn :: Monad m => [m a] -> m [a]
+seqn []         = return []
+seqn (act:acts) = do x <- act
+                     xs <- seqn acts
+                     return (x:xs)
