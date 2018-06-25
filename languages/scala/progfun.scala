30f06d32e3cc46ad9ab07c192cf7a0c3604e1eef
Jin Choi <jinchoiseoul@gmail.com>
2018-06-25 18:06:56 +0900

    add expr_tree
    diffrence btw polymorphism in OOP and pattern matching in FP
    anti-pattern `classification/accessor functions`

--- languages/scala/progfun.scala
+++ languages/scala/progfun.scala
@@ -141,6 +141,43 @@ object runnable {
     printEq("msort(List(3,1,5,4,2,1))", week4.sortings.msort(List(3,1,5,4,2,1)))
     printEq("qsort(List(3,1,5,4,2,1))", week4.sortings.qsort(List(3,1,5,4,2,1)))
 
+    horizontal_line()
+    val expr1 = new week4.expr_tree.Sum1(
+                  new week4.expr_tree.Sum1(
+                    new week4.expr_tree.Number1(1),
+                    new week4.expr_tree.Number1(2)),
+                  new week4.expr_tree.Sum1(
+                    new week4.expr_tree.Number1(3),
+                    new week4.expr_tree.Number1(4)))
+    printEq("Sum(Sum(Num(1),Num(2)),Sum(Num(3),Num(4)))", week4.expr_tree.Expr1.eval(expr1))
+
+    val expr2 = new week4.expr_tree.Sum2(
+                  new week4.expr_tree.Sum2(
+                    new week4.expr_tree.Number2(1),
+                    new week4.expr_tree.Number2(2)),
+                  new week4.expr_tree.Sum2(
+                    new week4.expr_tree.Number2(3),
+                    new week4.expr_tree.Number2(4)))
+    printEq("Sum(Sum(Num(1),Num(2)),Sum(Num(3),Num(4)))", week4.expr_tree.Expr2.eval(expr2))
+
+    val expr3 = new week4.expr_tree.Sum3(
+                  new week4.expr_tree.Sum3(
+                    new week4.expr_tree.Number3(1),
+                    new week4.expr_tree.Number3(2)),
+                  new week4.expr_tree.Sum3(
+                    new week4.expr_tree.Number3(3),
+                    new week4.expr_tree.Number3(4)))
+    printEq("Sum(Sum(Num(1),Num(2)),Sum(Num(3),Num(4)))", expr3.eval)
+
+    val expr4 = new week4.expr_tree.Sum4(
+                  new week4.expr_tree.Sum4(
+                    new week4.expr_tree.Number4(1),
+                    new week4.expr_tree.Number4(2)),
+                  new week4.expr_tree.Sum4(
+                    new week4.expr_tree.Number4(3),
+                    new week4.expr_tree.Number4(4)))
+    printEq("Sum(Sum(Num(1),Num(2)),Sum(Num(3),Num(4)))", expr4.eval)
+
     println("")
   }
 
@@ -454,5 +491,104 @@ object week4 {
         }
       }
     }
+  }
+  
+  object expr_tree {
+    // limitation of OOP by `accessor/classification functions` pattern
+    trait Expr1 {
+      // classification functions
+      def isNumber: Boolean
+      def isSum: Boolean
+      // accessor functions
+      def numValue: Int
+      def leftOp: Expr1
+      def rightOp: Expr1
+    }
+    class Number1(val n: Int) extends Expr1 {
+      override def isNumber: Boolean = true
+      override def isSum: Boolean = false
+      override def numValue: Int = n
+      override def leftOp: Nothing = throw new Error("num.leftOp")
+      override def rightOp: Nothing = throw new Error("num.rightOp")
+    }
+    class Sum1(val l: Expr1, val r: Expr1) extends Expr1 {
+      override def isNumber: Boolean = false
+      override def isSum: Boolean = true
+      override def numValue: Int = throw new Error("Sum.numValue")
+      override def leftOp: Expr1 = l
+      override def rightOp: Expr1 = r
+    }
+    object Expr1 {
+      // Writing and using classification / accessor functions become tedious quickly,
+      // especially when adding more subslasses of Expr1, e.g., Var, Prod, etc.
+      // quadratic increase of methods
+      def eval(e: Expr1): Int = {
+        if (e.isNumber) e.numValue
+        else if (e.isSum) eval(e.leftOp) + eval(e.rightOp)
+        else throw new Error("Unknown Expression " + e)
+      }
+    }
+
+    // A hacky solution to overcoming `classification/accessor functions` pattern - type testing/casting.
+    // This approach has nothing to do with polymorphism, which is the major reason for adopting OOP
+    trait Expr2
+    class Number2(val n: Int) extends Expr2
+    class Sum2(val leftOp: Expr2, val rightOp: Expr2) extends Expr2
+    object Expr2 {
+      // Type tests and casts: hacky, unsafe, low-level
+      def eval(e: Expr2): Int = {
+        if (e.isInstanceOf[Number2]) e.asInstanceOf[Number2].n
+        else if (e.isInstanceOf[Sum2]) eval(e.asInstanceOf[Sum2].leftOp ) + eval(e.asInstanceOf[Sum2].rightOp)
+        else throw new Error("Unknown Expressions " + e)
+      }
+    }
+
+    // Best solution 1
+    // Fully taking advantage of OOP(polymorphism) makes the classes extensible.
+    // It lacks classification/accessor fucntions (thanks to polymorphism).
+    // strong point: adding new subclass to trait is neat
+    // weak point: adding new api to trait is messy. Every existing subclasses needs to be modified.
+    trait Expr3 {
+      def eval: Int
+      def leftOp: Expr3
+      def rightOp: Expr3
+    }
+    class Number3(val n: Int) extends Expr3 {
+      override def eval: Int = n
+      override def leftOp: Expr3 = throw new Error("num.leftOp")
+      override def rightOp: Expr3 = throw new Error("num.rightOp")
+    }
+    class Sum3(val l: Expr3, val r: Expr3) extends Expr3 {
+      override def eval: Int = l.eval + r.eval
+      override def leftOp: Expr3 = l
+      override def rightOp: Expr3 = r
+    }
+
+    // Best Solution 2
+    // Fully taking advantage of pattern matching, also, makes the classes extensible.
+    // It lacks classification/accessor fucntions (thanks to pattern matching).
+    // pattern matching in functional languages is so beautiful as to abstract all those tedious work from OOP
+    // pattern matching is strong with extending features from existing classes
+    // whereas OOP is strong with adding more subclasses without touching existing api
+    // In conclusion, polymorphism in OOP and pattern matcing from FP compliment each other.
+    trait Expr4 {
+      def eval: Int = this match {
+        case Number4(n) => n
+        case Sum4(l, r) => l.eval + r.eval
+      }
+      def show: String = this match {
+        case Number4(n) => n.toString
+        case Sum4(l, r) => l.show + " + " + r.show
+      }
+    }
+    case class Number4(val n: Int) extends Expr4
+    case class Sum4(val l: Expr4, val r: Expr4) extends Expr4
+    // case class Prod(val l: Expr4, val r: Expr4) extends Expr4
+    // case object Var extends Expr4 // does case object work??
+    // object Expr4 {
+    //   //todo quiz: show ( Prod( Sum(2, Var), 4 ) == "(2 + x) * 4"
+    //   //todo quiz: show ( compress ( Sum(Sum(Sum(Var, Var), Var), 3) )  == " 3 * x + 3"
+    //   def compress(e: Expr4): Expr4 = ???
+    // }
   }
 }
