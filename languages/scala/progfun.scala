0c9c3295c1d621c9c988c0dd8091103a24ce5a5d
Jin Choi <jinchoiseoul@gmail.com>
2018-06-25 16:31:18 +0900

    add sortings

--- languages/scala/progfun.scala
+++ languages/scala/progfun.scala
@@ -1,5 +1,7 @@
 //Created by Jin
 
+import scala.annotation.tailrec
+
 object runnable {
   def main(args: Array[String]) {
     printTitle("week1")
@@ -133,6 +135,12 @@ object runnable {
     printAssert("three - two == one", three - two == one)
     printError("three - four", three - four, paren=true)
     
+    horizontal_line()
+    printEq("isort1(List(3,1,5,4,2,1))", week4.sortings.isort1(List(3,1,5,4,2,1)))
+    printEq("isort2(List(3,1,5,4,2,1))", week4.sortings.isort2(List(3,1,5,4,2,1)))
+    printEq("msort(List(3,1,5,4,2,1))", week4.sortings.msort(List(3,1,5,4,2,1)))
+    printEq("qsort(List(3,1,5,4,2,1))", week4.sortings.qsort(List(3,1,5,4,2,1)))
+
     println("")
   }
 
@@ -387,5 +395,64 @@ object week4 {
     override def isZero: Boolean = false
     override def +(that: Nat): Nat = (this predecessor) + (that successor)
     override def -(that: Nat): Nat = if (that isZero) this else this.predecessor - that.predecessor
-  }  
+  }
+
+  object sortings {
+    def isort1(xs: List[Int]): List[Int] = {
+      // By Martin
+      def insert(x: Int, ys: List[Int]): List[Int] = ys match {
+        case Nil => x::Nil
+        case y::ys1 => if (x <= y) x::ys else y::insert(x, ys1)
+      }
+      xs match {
+        case Nil => Nil
+        case y::ys => insert(y, isort1(ys))
+      }
+    }
+
+    def isort2(xs: List[Int]): List[Int] = {
+      // By Jin
+      def insert(x: Int, acc: List[Int]): List[Int] = acc match {
+        case Nil  => x :: Nil
+        case y::ys =>
+          if (x <= y) x :: acc
+          else y :: insert(x, ys)
+      }
+      @tailrec
+      def loop(acc: List[Int], xs: List[Int]): List[Int] = xs match {
+        case Nil => acc
+        case y::ys => loop(insert(y, acc), ys)
+      }
+      loop(Nil, xs)
+    }
+
+    def hsort(xs: List[Int]): List[Int] = ???
+
+    def qsort(xs: List[Int]): List[Int] = xs match {
+      case Nil => xs
+      case y::ys => {
+        val (l,r) = ys partition(_ <= y)
+        qsort(l) ++ (y :: qsort(r))
+      }
+    }
+
+    def msort(xs: List[Int]): List[Int] = {
+      def merge(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+        case (_, Nil) => xs
+        case (Nil, _) => ys
+        case (x :: xs1, y :: ys1) =>
+          if (x <= y) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+      xs match {
+        case Nil => xs
+        case x :: Nil => xs
+        case _ => {
+          val (l, r) = xs splitAt (xs.length / 2)
+          val (l2, r2) = (msort(l), msort(r))
+          merge(l2, r2)
+        }
+      }
+    }
+  }
 }
