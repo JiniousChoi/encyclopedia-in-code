63f0cda23e6d166ac1dec5a91114d78cb3190c58
Jin Choi <jinchoiseoul@gmail.com>
2018-07-02 10:18:16 +0900

    Create promise_for_dummies.js

---
+++ languages/nodejs/promise_for_dummies.js
@@ -0,0 +1,52 @@
+/* ES5, using Bluebird */
+var isMomHappy = true;
+
+//// Promise
+//var willIGetNewPhone = new Promise(
+//    function (resolve, reject) {
+//        if (isMomHappy) {
+//            var phone = {
+//                brand: 'Samsung',
+//                color: 'black'
+//            };
+//            resolve(phone);
+//        } else {
+//            var reason = new Error('mom is not happy');
+//            reject(reason);
+//        }
+//
+//    }
+//);
+
+// Promise
+var willIGetNewPhone = function () {
+    return new Promise(
+        function (resolve, reject) {
+            if (isMomHappy) {
+                var phone = {
+                    brand: 'Samsung',
+                    color: 'black'
+                };
+                resolve(phone);
+            } else {
+                var reason = new Error('mom is not happy');
+                reject(reason);
+            }
+        }
+    );
+}
+
+// call our promise
+var askMom = function () {
+    willIGetNewPhone()
+        .then(function (fulfilled) {
+            // yay, you got a new phone
+            console.log(fulfilled);
+        })
+        .catch(function (error) {
+            // ops, mom don't buy it
+            console.log(error.message);
+        });
+}
+
+askMom();
