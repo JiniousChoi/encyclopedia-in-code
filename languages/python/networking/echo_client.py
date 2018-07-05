e4c59f86980da70441210cc0bebe0f414e1b4d2f
JiniousChoi <jinchoiseoul@gmail.com>
2018-07-05 17:55:30 +0900

    add echo_server_by_select and its tester client

---
+++ languages/python/networking/echo_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import socket
+import threading as t
+from time import sleep
+
+server, port = 'localhost', 50000
+
+def sender_bot(_id):
+    msgs = ['hello, I am bot_{x}'.format(x=_id),
+            'nice to meet you_{x}'.format(x=_id)]
+    def __sender_bot():
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((server, port))
+        for msg in msgs:
+            sz = s.send(msg.encode())
+            print('-> {} : {}'.format(id(s), s.recv(sz).decode()))
+            sleep(1)
+        s.close()
+    bot_thread = t.Thread(target=__sender_bot)
+    bot_thread.start()
+
+bots = 10
+for i in range(1, bots+1):
+    sender_bot(i)
---
+++ languages/python/networking/echo_server_by_select.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+import select, socket, sys, Queue
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setblocking(0)
+server.bind(('localhost', 50000))
+server.listen(5)
+inputs = [server]
+outputs = []
+message_queues = {}
+
+while inputs:
+    readable, writable, exceptional = select.select(
+        inputs, outputs, inputs)
+    for s in readable:
+        if s is server:
+            connection, client_address = s.accept()
+            print('-- {} : CONNECTED '.format(id(connection)))
+            # https://docs.python.org/2/library/socket.html#socket.socket.setblocking
+            connection.setblocking(0)
+            inputs.append(connection)
+            message_queues[connection] = Queue.Queue()
+        else:
+            data = s.recv(1024)
+            if data:
+                print('<- {} : {}'.format(id(s), data))
+                message_queues[s].put(data)
+                if s not in outputs:
+                    outputs.append(s)
+            else:
+                print('<- {} : {}'.format(id(s), 'FIN'))
+                if s in outputs:
+                    outputs.remove(s)
+                inputs.remove(s)
+                s.close()
+                print('-> {} : {}'.format(id(s), 'ACK+FIN'))
+                del message_queues[s]
+
+    for s in writable:
+        try:
+            next_msg = message_queues[s].get_nowait()
+        except Queue.Empty:
+            outputs.remove(s)
+        else:
+            s.send(next_msg)
+            print('-> {} : {}'.format(id(s), next_msg))
+
+    for s in exceptional:
+        print('!! {} : {}'.format(id(s), 'exception occured'))
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+        del message_queues[s]
