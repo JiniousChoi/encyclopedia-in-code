742b764e5b8be0030e8ffbdf8693036e17819786
JiniousChoi <jinchoiseoul@gmail.com>
2018-07-18 10:32:48 +0900

    modify counter2.ex main logic

--- books/seven_concurrencies_in_7_weeks/actors/02_counter/counter2.ex
+++ books/seven_concurrencies_in_7_weeks/actors/02_counter/counter2.ex
@@ -6,7 +6,7 @@ defmodule Counter do
     ref = make_ref()
     send(counter, {:next, self(), ref})
     receive do
-      {:ok, ^ref, count} -> count
+      {:ok, ^ref, count} -> IO.puts(count)
     end
   end
   def loop(count) do
@@ -18,10 +18,16 @@ defmodule Counter do
   end
 end
 
+# main by jin
+counter = Counter.start(42)
+Counter.next(counter)
+Counter.next(counter)
+
+# ===== Simulation on elixir repl =====
 # iex(1)> counter = Counter.start(42)
 # #PID<0.63.0>
 # iex(2)> Counter.next(counter)
-# 42
+# 42")
 # iex(3)> Counter.next(counter)
 # 43
 
