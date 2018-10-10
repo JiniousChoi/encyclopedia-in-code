3af118fbc76b6599c8398e845bb6252a585fa8a2
JiniousChoi <jinchoiseoul@gmail.com>
2018-10-10 09:41:15 +0900

    add Lesson0504_HigherOrderListFns

---
+++ languages/scala/progfun/src/main/scala/week5/Lesson0504_HigherOrderListFns.scala
@@ -0,0 +1,47 @@
+package week5
+
+object Lesson0504_HigherOrderListFns {
+  object Mapping {
+    def squareListByPatternMatching(xs: List[Int]): List[Int] = xs match {
+      case Nil => Nil
+      case x::xs1 => x*x :: squareListByPatternMatching(xs1)
+    }
+
+    def squareListByMap(xs: List[Int]): List[Int] = xs map (x => x*x)
+  }
+
+  object Filtering {
+    def posElemsByPatternMatching(xs: List[Int]): List[Int] = xs match {
+      case Nil => Nil
+      case x::xs1 => if (x>0) x::posElemsByPatternMatching(xs1) else posElemsByPatternMatching(xs1)
+    }
+
+    def posElemsByFilter(xs: List[Int]): List[Int] = xs filter (_>0)
+  }
+
+  // more higher-order methods on list
+  // group 1: filter, filterNot, partition
+  // group 2: takeWhile, dropWhile, span
+  object exercise {
+    def pack[T](xs: List[T]): List[List[T]] = xs match {
+      case Nil => Nil
+      case x::xs1 => {
+        val (ys, zs) = xs span (_==x)
+        ys :: pack(zs)
+      }
+    }
+
+    def encodeByJin[T](xs: List[T]): List[(T, Int)] = {
+      def encode0[T](xss: List[List[T]]): List[(T, Int)] = xss match {
+        case List() => Nil
+        case xs::xss1 => (xs.head, xs.size) :: encode0(xss1)
+      }
+      val packed = pack(xs)
+      encode0(packed)
+    }
+
+    def encodeByMartin[T](xss: List[T]): List[(T, Int)] =
+      // all you need is a simple transformation on the result from pack function
+      pack(xss) map (xs => (xs.head, xs.length))
+  }
+}
---
+++ languages/scala/progfun/src/test/scala/week5/Lecture0504_HigherOrderListFnsTest.scala
@@ -0,0 +1,50 @@
+package week5
+
+import org.scalatest.FunSuite
+
+class Lecture0504_HigherOrderListFnsTest extends FunSuite{
+  val xs1 = List(-3,-2,0,1,2,3)
+  val xs1sq = List(9,4,0,1,4,9)
+  val xs1pos = List(1,2,3)
+
+  test("squareList") {
+    assert (Lesson0504_HigherOrderListFns.Mapping.squareListByPatternMatching(xs1) === xs1sq)
+    assert (Lesson0504_HigherOrderListFns.Mapping.squareListByMap(xs1) === xs1sq)
+  }
+
+  test("posElems") {
+    assert (Lesson0504_HigherOrderListFns.Filtering.posElemsByPatternMatching(xs1) === xs1pos)
+    assert (Lesson0504_HigherOrderListFns.Filtering.posElemsByFilter(xs1) === xs1pos)
+  }
+
+  test("more higher order methods on list - filter, filterNot, partition") {
+    val ys = List(1,2,-3,-4,5,6,-7,-8)
+    val ysPos = List(1,2,5,6)
+    val ysNotPos = List(-3,-4,-7,-8)
+    assert (ys.filter(_>0) === ysPos)
+    assert (ys.filterNot(_>0) === ysNotPos)
+    assert (ys.partition(_>0) === (ysPos, ysNotPos))
+  }
+
+  test("more higher order methods on list - takeWhile, dropWhile, span") {
+    val ys = List(1,2,-3,-4,5,6,-7,-8)
+    val ysTakenWhile = List(1,2)
+    val ysDroppedWhile = List(-3,-4,5,6,-7,-8)
+    assert (ys.takeWhile(_>0) === ysTakenWhile)
+    assert (ys.dropWhile(_>0) === ysDroppedWhile)
+    assert (ys.span(_>0) === (ysTakenWhile, ysDroppedWhile))
+  }
+
+  test("pack") {
+    val xs = List("a", "a", "a", "b", "c", "c", "a")
+    val xsPacked = List(List("a", "a", "a"), List("b"), List("c", "c"), List("a"))
+    assert (Lesson0504_HigherOrderListFns.exercise.pack(xs) === xsPacked)
+  }
+
+  test("encode") {
+    val input = List("a", "a", "a", "b", "c", "c", "a")
+    val expected = List(("a", 3), ("b", 1), ("c", 2), ("a", 1))
+    assert (Lesson0504_HigherOrderListFns.exercise.encodeByJin(input) === expected)
+    assert (Lesson0504_HigherOrderListFns.exercise.encodeByMartin(input) === expected)
+  }
+}
