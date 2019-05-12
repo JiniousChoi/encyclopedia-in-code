cdcbbb41927b154e82181b4c3a1f8e95ee267767
JiniousChoi <jinchoiseoul@gmail.com>
2019-05-13 08:34:42 +0900

    [lang/clj] add 1067s solution to combi

--- languages/clojure/combi.clj
+++ languages/clojure/combi.clj
@@ -1,6 +1,6 @@
 ; http://www.4clojure.com/problem/solutions/103
 
-; my solution; O(nCr)
+; my solution; O(nCr); is it the most efficient among the solutions here
 (defn combis [[x & t :as xs] r]
  (cond (or (nil? x) (empty? xs) (< r 0) (< (count xs) r)) ()
        (zero? r) '(())
@@ -8,16 +8,24 @@
        :else (concat (map #(cons x %) (combis t (dec r)))
                      (combis t r))))
 
+; 1067's solution; O(nCr)
+(fn combi [r xs]
+  (if (zero? r)
+    #{#{}}
+    (set (for [x xs
+               s (combi (dec r) (disj xs x))]
+           (conj s x)))))
+
 ; daowen's solution; O(nCr)
-(defn k-combos [n items]
-  (if (zero? n) #{#{}}
+(fn combi [r items]
+  (if (zero? r) [#{}]
     ((comp set mapcat)
-      #(for [i (k-combos (dec n) (disj items %))] (conj i %))
+      #(for [i (combi (dec r) (disj items %))] (conj i %))
       items)))
 
 ; maximental's solution; O(|s|**r); 비효율적
 ; 특징 tail recursion
-(defn your-combi [n s]
+(defn combi [n s]
   (loop [n n a #{#{}}]
     (if (> n 0)
       (recur (dec n) (set (for [x a y s :when (not (x y))] (conj x y))))
